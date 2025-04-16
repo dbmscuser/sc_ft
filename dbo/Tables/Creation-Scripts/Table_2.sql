68ac83869a84b7121a5e242172713572fd8be55c
dbmscuser <dbmscuser>
2025-04-16 15:38:21 +0000

    Missed a column; TaskID: V2025.03

--- dbo/Tables/Creation-Scripts/Table_2.sql
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -1,4 +1,5 @@
 CREATE TABLE [dbo].[Table_2] (
-    [FU]  NCHAR (10) NULL,
-    [Bar] NCHAR (10) NULL
+    [FU]       NCHAR (10) NULL,
+    [Bar]      NCHAR (10) NULL,
+    [OnceMore] NCHAR (10) NULL
 );
